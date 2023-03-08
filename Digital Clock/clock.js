b47a1375c8c4f0d40e376015e9fc309fc81f5c94
Gaurav Kumar <91934323+gauravkr7992@users.noreply.github.com>
2023-03-08 11:40:02 +0530

    Add files via upload

---
+++ Digital Clock/clock.js
@@ -0,0 +1,24 @@
+const hr=document.getElementById("hours");
+const min=document.getElementById("minutes");
+const sec=document.getElementById("seconds");
+const ap=document.getElementById("ampm");
+function updateclock(){
+    let h=new Date().getHours();
+    let m=new Date().getMinutes();
+    let s=new Date().getSeconds();
+    let ampm="AM";
+    if(h>12){
+        h=h-12;
+        ampm="PM";
+    }
+    h=h<10?"0"+h:h;
+    m=m<10?"0"+m:m;
+    s=s<10?"0"+s:s;
+    hr.innerText=h;
+    min.innerText=m;
+    sec.innerText=s;
+    ap.innerText=ampm; 
+    setTimeout(()=>{updateclock()},1000)
+
+}
+updateclock();
